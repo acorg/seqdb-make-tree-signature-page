53edadb1f9a7239a6a4c2f04375d1751522faea5
Eugene Skepner <github@skepner.eu>
2016-05-30 16:37:59 +0200

    fasttree support development

--- python/seqdb/__init__.py
+++ python/seqdb/__init__.py
@@ -1,7 +1,7 @@
 from seqdb_backend import *
 from .timeit import timeit
 from .fasta import export_from_seqdb
-from .make_tree import run_raxml_best_garli, run_raxml_survived_best_garli, run_raxml_all_garli, run_in_background
+from .make_tree import run_raxml_best_garli, run_raxml_survived_best_garli, run_raxml_all_garli, run_fasttree_raxml_survived_best_garli, run_in_background
 from .update import SeqdbUpdater
 
 # ----------------------------------------------------------------------
