5bf5c1f2d4550a71bd3e9c6b78e8b6ec8e10676d
Eugene Skepner <github@skepner.eu>
2016-09-01 09:08:14 +0200

    script to postprocess garli results generated by GH/ssm-report

--- python/seqdb/tree_maker.py
+++ python/seqdb/tree_maker.py
@@ -87,6 +87,8 @@ class Results:
         data = json.loadf(filepath)
         for k, v in data.items():
             setattr(r, k, v)
+        if getattr(r, "results", None) and isinstance(r.results[0], str)
+            r.results = []                # bug in output, ignore it
         return r
 
 # ----------------------------------------------------------------------
